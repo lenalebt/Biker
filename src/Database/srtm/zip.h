774acaf1612caab83e722fa0d9620e00e5a0bd22
Lena Brüder <lena_lebt@gmx.de>
2011-03-29 13:10:42 +0200

    GPL-Disclaimer vor jede Datei getan

--- src/Database/srtm/zip.h
+++ src/Database/srtm/zip.h
@@ -1,7 +1,25 @@
-/* Copyright (c) 2009 Hermann Kraus
- * This software is available under a "MIT Style" license
- * (see COPYING).
- */
+/*
+    Biker wants to be a routing software, intended to be useful for planning bike tours.
+    Copyright (C) 2011  Lena Brueder
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    This file was originally taken from Hermann Kraus (c) 2009 and was available
+    under a "MIT Style" license, see COPYING in this folder.
+*/ 
+
+
 /** \file
     Read SRTM zip files. */
 #ifndef __SRTM_ZIP_H__
